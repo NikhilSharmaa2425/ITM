445dae922e20f2a06e3fec74c72d4e6f51d24d69
Nikhil Sharma <nikhil.sharmaa.2425@gmail.com>
2024-09-23 12:58:47 +0530

    javascript hash-set (set)

---
+++ javascript/230924/script.js
@@ -0,0 +1,16 @@
+// set
+
+const myset = new Set([1,2,3,4,5,6,7]);
+console.log(myset)
+
+myset.add("abc")
+console.log(myset)
+
+// myset.clear() clears all the set
+
+// myset.delete() deleet the whole set 
+
+// myset.has() checks for a specific value in a set 
+
+
+console.log(myset.entries())
