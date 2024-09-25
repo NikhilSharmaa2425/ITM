b3f8ddc9f8cc04b0a460a856cecab1db2932f36b
Nikhil Sharma <nikhil.sharmaa.2425@gmail.com>
2024-09-25 13:22:10 +0530

    new files for 25th of 09 2024, more filess

--- javascript/2509/index.js
+++ javascript/2509/index.js
@@ -1,12 +1,26 @@
-const http = require("http")
+// const http = require("http")
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hell yeah, i am live at server')
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/html'});
+//     res.end('Hell yeah, i am live at server')
+// })
+
+
+
+// server.listen(3000,()=>{
+//     console.log("server is connected")
+// })
+
+const express = require("express")
+
+const app = express()
+
+//5 methods - post get put delete patch
+
+app.get("/getdata",(req,res)=>{
+    res.send("hi there")
 })
 
-
-
-server.listen(3000,()=>{
+app.listen(3000,()=>{
     console.log("server is connected")
 })
